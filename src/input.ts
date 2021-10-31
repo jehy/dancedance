72222089757f6ca0e48eeb901006e3abf85f3e2e
Jehy <npm@jehy.ru>
2021-10-31 12:28:17 +0300

    first version

---
+++ src/input.ts
@@ -0,0 +1,66 @@
+import yargs from 'yargs/yargs';
+import { cpus } from 'os';
+
+export type Input = {
+  concurrency: number | string,
+  token: string,
+  keepDirs?: boolean,
+  inputDir: string,
+  inputMask: string,
+  outputDir: string,
+  server: 'jehy' | 'local',
+  album? : string,
+  skipExisting?: boolean,
+  customServer?: string,
+};
+
+export const argv = yargs(process.argv.slice(2))
+  .options({
+    concurrency: {
+      alias: 'c',
+      type: 'number',
+      required: false,
+      default: cpus().length,
+    },
+    token: {
+      alias: 't',
+      required: true,
+      type: 'string',
+    },
+    keepDirs: {
+      alias: 'k',
+      required: false,
+      type: 'string',
+    },
+    inputDir: {
+      alias: 'i',
+      type: 'string',
+    },
+    inputMask: {
+      alias: 'm',
+      type: 'string',
+      default: '**/*.mp3',
+    },
+    outputDir: {
+      alias: 'o',
+      type: 'string',
+    },
+    album: {
+      alias: 'a',
+      type: 'string',
+    },
+    server: {
+      choices: ['jehy', 'local'],
+      alias: 's',
+      default: 'local',
+    },
+    skipExisting: {
+      type: 'boolean',
+      default: false,
+    },
+    customServer: {
+      type: 'string',
+      default: '',
+    },
+  })
+  .help('help').parseSync() as Input;
